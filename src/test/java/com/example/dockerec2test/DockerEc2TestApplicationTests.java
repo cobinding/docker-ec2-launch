ddc39d947ceb3c5081af1d3d37c2d9579ce6aaa2
cobinding <cntpqls0218@gmail.com>
2023-08-16 21:47:45 +0900

    first commit.

---
+++ src/test/java/com/example/dockerec2test/DockerEc2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerec2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerEc2TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
